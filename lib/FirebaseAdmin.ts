6c7e94e47b30068432ffcf2e536f952e471c9f53
Progalt <henryrgronow@gmail.com>
2025-05-26 16:27:06 +0100

    Removed replace on admin private key

--- lib/FirebaseAdmin.ts
+++ lib/FirebaseAdmin.ts
@@ -7,7 +7,7 @@ if (!admin.apps.length) {
   const serviceAccount = {
     projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
     clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines
+    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY, // Replace escaped newlines
   };
 
   admin.initializeApp({
